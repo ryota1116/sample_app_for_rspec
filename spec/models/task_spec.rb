055e1c72a9833f8e7e8542ddbdcade5ee725f198
ryota1116 <ryota1116funa@gmail.com>
2020-05-04 14:16:30 +0900

    [Fix]Taskモデルのバリデーションチェックのテストを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,17 +10,20 @@ RSpec.describe Task, type: :model do
   it 'titleが存在しない場合無効であること' do
     task = build(:task, title: nil)
     expect(task).to be_invalid
+    expect(task.errors[:title]).to include("can't be blank")
   end
 
   it 'statusが存在しない場合無効であること' do
     task = build(:task, status: nil)
     expect(task).to be_invalid
+    expect(task.errors[:status]).to include("can't be blank")
   end
 
   it 'titleが重複する場合無効であること' do
     task = create(:task, title: 'タイトル')
     other_task = build(:task, title: 'タイトル')
     expect(other_task).to be_invalid
+    expect(other_task.errors[:title]).to include('has already been taken')
   end
 
   it 'titleが重複しない場合有効であること' do
